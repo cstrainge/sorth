7a30dcc13d905cd83cee6563d769fd798508acb4
Chloë Strainge <nullptr.0@gmail.com>
2023-08-07 11:00:07 -0700

    Updated the comments.

--- tests/02_test_structs.f
+++ tests/02_test_structs.f
@@ -1,19 +1,26 @@
 
-( Now lets test data object creation and access. )
+( Define our structures... )
 # foo a b c ;
 # bar x y z ;
 
+( Create a variable to hold an instance of foo. )
 variable fp
 foo.new fp !
 
+( Print out what we have so far. )
 "---" . fp ?
 
+( Assign some values to the first two fields. )
 1024 foo.a fp #!!
 "Hello world!" foo.b fp #!!
 
+( Create an instance of bar and write it into foo.c )
 bar.new foo.c fp #!!
+
+( Now fill out the rest of foo.c's bar fields. )
 150 bar.x foo.c fp #@@ #!
 250 bar.y foo.c fp #@@ #!
 350 bar.z foo.c fp #@@ #!
 
+( Finally print the whole thing. )
 "---" . fp ?
