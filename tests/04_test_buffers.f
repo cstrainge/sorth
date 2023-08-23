d3febda284ab23d4699a0224c86b75f808d406f0
Chloë Strainge <nullptr.0@gmail.com>
2023-08-22 20:13:36 -0700

    Added more tests.

--- tests/04_test_buffers.f
+++ tests/04_test_buffers.f
@@ -1,14 +1,16 @@
 
 ( Create a byte buffer of 50 bytes. )
 variable bytes
-50 buffer.new bytes !
+70 buffer.new bytes !
 
 
 ( Write a few values into the buffer. )
 -1 bytes buffer.i32!!
 1024.28 bytes buffer.f32!!
 2048.56 bytes buffer.f64!!
-"Hello world." 12 bytes buffer.string!!
+"Hello world." 16 bytes buffer.string!!
+"Something wicked this way comes." 32 bytes buffer.string!!
+"foo" 5 bytes buffer.string!!
 
 
 ( Show the buffer as it is now. )
@@ -25,7 +27,9 @@ cr
 "i32:" . bytes buffer.i32@@ .cr
 "f32:" . bytes buffer.f32@@ .cr
 "f64:" . bytes buffer.f64@@ .cr
-"Str:" . 12 bytes buffer.string@@ .cr
+"Str:" . 16 bytes buffer.string@@ .cr
+"Str:" . 32 bytes buffer.string@@ .cr
+"Str:" . 5 bytes buffer.string@@ .cr
 
 
 ( Move the buffer back to position 0 and read the first value again this time as an unsigned int. )
