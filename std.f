c521a6c8ec0e91b64283d7871a9d4005bd444ee7
Chloë Strainge <nullptr.0@gmail.com>
2023-08-27 16:30:02 -0700

    Commented the repl.

--- std.f
+++ std.f
@@ -390,16 +390,24 @@
     cr
 
     begin
+        ( Loop forever, because when the user enters the quit command the interpreter will exit )
+        ( gracefully on it's own. )
         true
     while
         try
+            ( Always make sure we get the newest version of the prompt.  That way the user can )
+            ( change it at runtime. )
             cr ` prompt execute
 
+            ( Get the text from the user and execute it.  We are just using a really simple )
+            ( implementaion of readline for now. )
             readline
             code.execute_source
 
+            ( If we got here, everything is fine. )
             "ok" .cr
         catch
+            ( Something in the user code failed, display the error and try again. )
             .cr
         endcatch
     repeat
