1ad0ab8717e24dd85009f1a23b1bb91c1fbc87dc
Chloë Strainge <nullptr.0@gmail.com>
2023-09-06 10:39:43 -0700

    Updated array test.

--- tests/06_test_arrays.f
+++ tests/06_test_arrays.f
@@ -1,7 +1,7 @@
 
 5 [].new variable! my_array
 
-"Original:       " . my_array ?
+"Original:           " . my_array ?
 
 10 my_array [ 0 ]!!
 20 my_array [ 1 ]!!
@@ -9,17 +9,20 @@
 40 my_array [ 3 ]!!
 50 my_array [ 4 ]!!
 
-"Updated:        " . my_array @ .cr
+"Updated:            " . my_array @ .cr
 
 
-"Read [1]:       " . my_array [ 1 ]@@ .cr
-"Read [3]:       " . my_array [ 3 ]@@ .cr
+"Read [1]:           " . my_array [ 1 ]@@ .cr
+"Read [3]:           " . my_array [ 3 ]@@ .cr
 
-"Read [ 1 3 2 ]: " . my_array [ 1 , 3 , 2 ]@@ . . .cr
-"Write [ 1 3 2 ]:" . 1024 2048 4096 my_array [ 1 , 3 , 2 ]!! my_array ?
+"Read [ 1 3 2 ]:     " . my_array [ 1 , 3 , 2 ]@@ . . .cr
+"Write [ 1 3 2 ]:    " . 1024 2048 4096 my_array [ 1 , 3 , 2 ]!! my_array ?
 
 10 my_array [].size!!
-"Resize bigger:  " . my_array ?
+"Resize bigger:      " . my_array ?
 
 2 my_array [].size!!
-"Resize smaller: " . my_array ?
+"Resize smaller:     " . my_array ?
+
+
+"Create on the spot: " . [ 1024 , 2048 , 4096 ] .cr
