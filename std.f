c6633caafc7f8044d4a6bf4c78ba25bc46ea5ec3
Chloë Strainge <nullptr.0@gmail.com>
2023-09-06 10:36:02 -0700

    Added the array creation syntax [ value , ... ]

--- std.f
+++ std.f
@@ -490,7 +490,9 @@
 
 
 
-: [ immediate description: "Syntax definition for 'array [ index or indices ]' access."
+: [ immediate
+    description: "Define 'array [ index or indices ]' access or `[ value , ... ]` creation."
+
     1 variable! index_count
     1 [].new variable! index_blocks
 
@@ -498,11 +500,12 @@
 
     false variable! found_end_bracket
     true variable! is_writing
+    false variable! is_creating
 
     code.new_block
 
     begin
-        "," "]!" "]!!" "]@" "]@@" 5 code.compile_until_words
+        "," "]!" "]!!" "]@" "]@@" "]" 6 code.compile_until_words
 
         code.pop_stack_block index_count @ -- index_blocks []!!
 
@@ -514,58 +517,88 @@
                 index_count @ index_blocks [].size!!
             endof
 
-            "]!"  of  ` []!  command !  true found_end_bracket !  endof
-            "]!!" of  ` []!! command !  true found_end_bracket !  endof
-            "]@"  of  ` []@  command !  true found_end_bracket !  false is_writing !  endof
-            "]@@" of  ` []@@ command !  true found_end_bracket !  false is_writing !  endof
+            "]"   of  ` [].new command !  true found_end_bracket !  true is_creating !  endof
+            "]!"  of  ` []!  command !    true found_end_bracket !                      endof
+            "]!!" of  ` []!! command !    true found_end_bracket !                      endof
+            "]@"  of  ` []@  command !    true found_end_bracket !  false is_writing !  endof
+            "]@@" of  ` []@@ command !    true found_end_bracket !  false is_writing !  endof
         endcase
 
         found_end_bracket @
     until
 
-    index_count @ 1 =
+    is_creating @
     if
-        0 index_blocks []@@ code.push_stack_block
-
-        ` swap op.execute
+        index_count @ op.push_constant_value
         command @ op.execute
 
-        code.merge_stack_block
-    else
-        index_count @ -- variable! i
+        0 index_count !
 
         begin
-            i @ index_blocks []@@ code.push_stack_block
+            index_count @ index_blocks [].size@@ <
+        while
+            index_count @ index_blocks []@@ code.push_stack_block
 
-            is_writing @
+            code.stack_block_size@ 0>
             if
-                true code.insert_at_front
-                ` over op.execute
-                false code.insert_at_front
-            else
                 true code.insert_at_front
                 ` dup op.execute
                 false code.insert_at_front
+
+                ` swap op.execute
+                index_count @ op.push_constant_value
+                ` swap op.execute
+                ` []! op.execute
             then
+
+            code.merge_stack_block
+            index_count ++!
+        repeat
+    else
+        index_count @ 1 =
+        if
+            0 index_blocks []@@ code.push_stack_block
 
             ` swap op.execute
             command @ op.execute
 
-            is_writing @ true <>
-            if
-                ` swap op.execute
-            then
-
             code.merge_stack_block
+        else
+            index_count @ -- variable! i
 
-            i --!
-            i @ 0<
-        until
-        ` drop op.execute
+            begin
+                i @ index_blocks []@@ code.push_stack_block
+
+                is_writing @
+                if
+                    true code.insert_at_front
+                    ` over op.execute
+                    false code.insert_at_front
+                else
+                    true code.insert_at_front
+                    ` dup op.execute
+                    false code.insert_at_front
+                then
+
+                ` swap op.execute
+                command @ op.execute
+
+                is_writing @ true <>
+                if
+                    ` swap op.execute
+                then
+
+                code.merge_stack_block
+
+                i --!
+                i @ 0<
+            until
+            ` drop op.execute
+        then
     then
 ;
 
-: , immediate description: "Separator in the [ index , index , ... ] syntax."
+: , immediate description: "Separator in the [ index , ... ] and { key -> value , ... } syntaxes."
     sentinel_word
 ;
 
