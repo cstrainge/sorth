7d02012cd6ca31d41c88599f55a5827a3925bd6f
Chloë Strainge <nullptr.0@gmail.com>
2024-09-08 13:10:56 -0700

    Made the bytecode display a little nicer.

--- src/builtin_words.cpp
+++ src/builtin_words.cpp
@@ -99,6 +99,16 @@ namespace sorth
 
                             stream << code[i].id << "  " << stringify(string) << std::endl;
                         }
+                        else if (  (   (code[i].id == OperationCode::Id::mark_loop_exit)
+                                    || (code[i].id == OperationCode::Id::mark_catch)
+                                    || (code[i].id == OperationCode::Id::jump_if_zero)
+                                    || (code[i].id == OperationCode::Id::jump_if_not_zero))
+                                 && (is_numeric(code[i].value)))
+                        {
+                            auto offset = as_numeric<int64_t>(interpreter, code[i].value);
+
+                            stream << code[i] << " -> " << i + offset << std::endl;
+                        }
                         else
                         {
                             stream << code[i] << std::endl;
