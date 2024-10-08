a9c8a363282719dca4b2fa2b01f9e4256c1e2681
Chloë Strainge <nullptr.0@gmail.com>
2024-10-07 21:51:55 -0700

    Refactored some of the type checks.

--- std.f
+++ std.f
@@ -473,56 +473,57 @@
 
 
 
+: value.both-are? description: "Check if the two values are the same type."
+                  signature: "a b value-check -- are-same-type?"
+    variable! operation
+    variable! b
+    variable! a
+
+    a @  operation @  execute
+    b @  operation @  execute
+    &&
+;
+
+
+
 : value.both-are-hash-tables? description: "Are two values hash tables?"
                               signature: "a b -- are-hash-tables?"
-         value.is-hash-table?
-    swap value.is-hash-table?
-    &&
+    ` value.is-hash-table?  value.both-are?
 ;
 
 
 
 : value.both-are-arrays? description: "Are two values arrays?"
                          signature: "a b -- are-arrays?"
-         value.is-array?
-    swap value.is-array?
-    &&
+    ` value.is-array?  value.both-are?
 ;
 
 
 
 : value.both-are-structures? description: "Are two values structures?"
                              signature: "a b -- are-structures?"
-         value.is-structure?
-    swap value.is-structure?
-    &&
+    ` value.is-structure?  value.both-are?
 ;
 
 
 
 : value.both-are-strings? description: "Are two values strings?"
                           signature: "a b -- are-strings?"
-         value.is-string?
-    swap value.is-string?
-    &&
+    ` value.is-string?  value.both-are?
 ;
 
 
 
 : value.both-are-numbers? description: "Are two values numbers?"
                           signature: "a b -- are-numbers?"
-         value.is-number?
-    swap value.is-number?
-    &&
+    ` value.is-number?  value.both-are?
 ;
 
 
 
 : value.both-are-booleans? description: "Are two values booleans?"
                            signature: "a b -- are-boolean?"
-         value.is-boolean?
-    swap value.is-boolean?
-    &&
+    ` value.is-boolean?  value.both-are?
 ;
 
 
